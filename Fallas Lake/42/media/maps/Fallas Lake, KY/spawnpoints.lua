77348d1603fd56c02a6af00da93cb303e60b0e18
MandoDB <77023822+MandoDB@users.noreply.github.com>
2024-12-20 00:33:18 +0100

    Add files via upload

---
+++ Fallas Lake/42/media/maps/Fallas Lake, KY/spawnpoints.lua
@@ -0,0 +1,7 @@
+function SpawnPoints()
+return {
+  unemployed = {
+    { posX = 7187, posY = 8345, posZ = 0 },
+  }
+}
+end
---
+++ Irvington/common/media/maps/Irvington, KY/spawnpoints.lua
@@ -0,0 +1,7 @@
+function SpawnPoints()
+  return {
+    unemployed = {
+      { posX = 2372, posY = 14265, posZ = 0 },
+    }
+  }
+  end
